b86f3c85a2987e96fc33fee610943f3b1e8c2331
ishaankadam <ishaankadam@192.168.113.123>
2022-12-21 11:26:16 +0530

    Change from Collaborator

--- GitPractise/src/com/cdac/Hii.java
+++ GitPractise/src/com/cdac/Hii.java
@@ -5,6 +5,7 @@ public class Hii {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hey there!!");
+System.out.println("HI from collaborator");
 	}
 
 }
